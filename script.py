93024f05cdd926c142d060b5c6cd91a78bbb8322
Caiosev <79454480+Caiosev@users.noreply.github.com>
2021-02-22 21:19:56 -0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,43 @@
+import time
+import pyautogui
+
+print("iniciando votação")
+votos = 0
+
+print(" ____   ____   ____    ___  _____ ")
+print("|  _ \ |  _ \ |  _ \  / _ \(_   _)")
+print("| |_) )| |_) )| |_) )| | | | | |  ")
+print("|  _ ( |  _ ( |  _ ( | | | | | |  ")
+print("| |_) )| |_) )| |_) )| |_| | | |  ")
+print("|____/ |____/ |____/  \___/  |_|  ")
+print("By: @caiosev")
+time.sleep(2)
+while True:
+
+    print("Aguarde....")
+    nome_voto = pyautogui.locateOnScreen('nome.png')
+    if nome_voto == None:
+        print("Nome NAO encontrado")
+        break
+    if nome_voto:
+        print("Nome encontrado")
+        point = pyautogui.center(nome_voto)
+        pyautogui.click(point)
+        check = pyautogui.locateOnScreen('check.png')
+        if check == None:
+            print("Captcha NAO Encontrado")
+            check = pyautogui.locateOnScreen('check.png')
+        if check:
+            print("Captcha Encontrado")
+            pointCheck = pyautogui.center(check)
+            pyautogui.click(pointCheck)
+            time.sleep(5)
+            pyautogui.hotkey('F5')
+            print("Voto Computado")
+            votos = votos + 1
+            print("Voce votou: ", votos, "vezes")
+            time.sleep(5)
+
+
+
+
