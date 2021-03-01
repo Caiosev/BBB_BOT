b0088ff055e142252d6b2b512b211de9ff946e30
Caio Severino <kaiosev9@gmail.com>
2021-03-01 11:55:59 -0300

    Comentários no Código

--- script.py
+++ script.py
@@ -11,32 +11,32 @@ print("|  _ ( |  _ ( |  _ ( |  _ ( | | | | | |  ")
 print("| |_) )| |_) )| |_) )| |_) )| |_| | | |  ")
 print("|____/ |____/ |____/ |____/  \___/  |_|  ")
 print("By: @caiosev")
-time.sleep(2)
+time.sleep(2) # Delay de 2 Seg
 while True:
 
     print("Aguarde....")
-    nome_voto = pyautogui.locateOnScreen('nome.png')
-    if nome_voto == None:
-        print("Nome NAO encontrado")
+    nome_voto = pyautogui.locateOnScreen('nome.png') #Localizando imagem na sua tela e colocando na variavel
+    if nome_voto == None: # Se a imagem nao for encontrada 
+        print("Nome NAO encontrado") 
         break
-    if nome_voto:
+    if nome_voto: # Se a imagem for encontrada
         print("Nome encontrado")
-        point = pyautogui.center(nome_voto)
-        pyautogui.click(point)
-        check = pyautogui.locateOnScreen('check.png')
-        if check == None:
+        point = pyautogui.center(nome_voto) # Recebendo a cooodenadas das imagem e colocando o mouse em cima
+        pyautogui.click(point) # Fazendo evento de Click
+        check = pyautogui.locateOnScreen('check.png') # Localizando Captcha na Tela
+        if check == None: # Se Captcha nao for encontrado
             print("Captcha NAO Encontrado")
-            check = pyautogui.locateOnScreen('check.png')
-        if check:
+            check = pyautogui.locateOnScreen('check.png') # Tentando Localizar Captcha Novamente
+        if check: # Se Captcha for encontrado
             print("Captcha Encontrado")
-            pointCheck = pyautogui.center(check)
-            pyautogui.click(pointCheck)
-            time.sleep(5)
-            pyautogui.hotkey('F5')
+            pointCheck = pyautogui.center(check) # Apontando o mouse para o Captch
+            pyautogui.click(pointCheck) # Evento de Clipe
+            time.sleep(5) # Delay de 5 seg para atulizar a pag 
+            pyautogui.hotkey('F5') # atualizando a pag
             print("Voto Computado")
-            votos = votos + 1
+            votos = votos + 1 # Computando Quantidade de votos
             print("Voce votou: ", votos, "vezes")
-            time.sleep(5)
+            time.sleep(5) # Delay de 5 seg
 
 
 
